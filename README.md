178d93da3c4b8beeae4050175e8636b02c0ff37b
luis <henrique3.terceiro@gmail.com>
2024-09-20 20:20:16 -0300

    Boiler plate ready

--- README.md
+++
@@ -1,2 +0,0 @@
-# aula-11-1tdspy-2sem
-Boyler plate NEXT.js
---
+++ meu-projeto/src/components/Rodape/Rodape.tsx
@@ -0,0 +1,10 @@
+
+export default function Rodape() {
+  return (
+    <footer>
+        <div>
+            <h1>Rodape</h1>
+        </div>
+        </footer>
+  )
+}
