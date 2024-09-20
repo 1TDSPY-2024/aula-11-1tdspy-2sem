0cbc45d9d5f299e522b4a91a53646a30c62df8ed
viniciuslabernardes <vinicius.l.a.bernardes@outlook.com>
2024-09-20 20:16:47 -0300

    Boyler plate pronto, rota estática criada

---
+++ aula11/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,10 @@
+
+import Menu from "@/components/Menu/Menu";
+export default function Cabecalho(){
+    return(
+        <header>
+            <h2>cabecalho</h2>
+            <Menu/>
+        </header>
+    );
+}
