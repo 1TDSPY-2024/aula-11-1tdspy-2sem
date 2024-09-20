89eca28720642ecb09f23c4d05bcab677f07f883
matheusGmota <livematgreg@gmail.com>
2024-09-20 20:57:59 -0300

    Lista de produtos

---
+++ aula11/src/app/produtos/produtos.ts
@@ -0,0 +1,8 @@
+export const produtos = [
+    {id:1, nome: "Cadeira", preco: 100, qtd:3},
+    {id:2, nome: "MousePad", preco: 300, qtd:1},
+    {id:3, nome: "Teclado", preco: 400, qtd:1},
+    {id:4, nome: "Fone de Ouvido", preco: 700, qtd:2},
+    {id:5, nome: "Mouse", preco: 100, qtd:5},
+    {id:6, nome: "Monitor", preco: 1500, qtd:1},
+]
