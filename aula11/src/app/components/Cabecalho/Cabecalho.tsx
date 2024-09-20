8314805662c5390094b8c4ff2fc3be72a04f7a3b
lopeslyra10 <augustolopeslyra@gmail.com>
2024-09-20 20:18:03 -0300

    Boyler plate estrurado

---
+++ aula11/src/app/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,10 @@
+export default function Cabecalho () {
+    return (
+        <header>
+            <h1>
+                Opa
+            </h1>
+        </header>
+    )
+  }
+  
---
+++ aula11/src/app/components/Menu/Menu.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Menu () {
+    return (
+        <nav>
+            <ul>
+                <li> <Link href="/aula11/src/app/components/Cabecalho">Home</Link> </li>
+            </ul>
+        </nav>
+    )
+  }
+  
